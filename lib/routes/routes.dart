2644cc080dadfda50c4270533b83be506e05ee4a
Didi17ane <didiane.koffi@epitech.eu>
2026-09-14 09:12:30 +0000

    [New branche]

--- lib/routes/routes.dart
+++ lib/routes/routes.dart
@@ -14,7 +14,7 @@ final routerProvider = Provider<GoRouter>((ref) {
   final authNotifier = ref.watch(routerAuthNotifierProvider);
 
   return GoRouter(
-    initialLocation: '/', 
+    initialLocation: '/',
     refreshListenable: authNotifier,
     redirect: (BuildContext context, GoRouterState state) {
       final bool isConnected = authNotifier.isConnected;
